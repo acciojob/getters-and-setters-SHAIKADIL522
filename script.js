a149a99016596b975356e4e6f5603b38957d6303
ADIL <adilshaik9112@gmail.com>
2026-02-12 19:33:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-//complete this code
+// Person class with constructor, getter, and setter
 class Person {
-   constructor(name,age){
-	   this._name = name;
-	   this._age = age;
-   }
-	get name(){
-		return this._name;
-	}
-	get age(){
-		return this._age;
-	}
+  constructor(name, age) {
+    this._name = name;  // Store name in a private property
+    this._age = age;    // Store age in a private property
+  }
+  
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+  
+  // Getter for age (needed to read the age)
+  get age() {
+    return this._age;
+  }
+  
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
 }
 
+// Student class inherits from Person
 class Student extends Person {
-	study(){
-		console.log(`${this.name}is studying`);
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
+// Teacher class inherits from Person
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`);
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
